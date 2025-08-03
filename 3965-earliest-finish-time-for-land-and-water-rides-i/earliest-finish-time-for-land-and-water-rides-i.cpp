4f1cef12a1eb2eb462aa1c2e31c4dba23d63f2ea
Siddhesh Pisal <82213191+Siddhesh202@users.noreply.github.com>
2025-08-03 13:52:56 +0530

    Time: 15 ms (40.00%) | Memory: 92 MB (40.00%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
-        int m = landStartTime.size();
-        int n = waterStartTime.size();
+    int earliestFinishTime(vector<int>& LS, vector<int>& LD, vector<int>& WS, vector<int>& WD) {
+        int m = LS.size();
+        int n = WS.size();
         int ans = INT_MAX;
 
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                   
                 // case 1 land then water
-                int time = landStartTime[i] + landDuration[i];
-                time = max(time, waterStartTime[j]) + waterDuration[j];
-
-                ans = min(time, ans);
+                int finishLandWater = max(LS[i]+LD[i], WS[j]) + WD[j];
 
                 // case 2 water then land
-                time = waterStartTime[j] + waterDuration[j];
-                time = max(time, landStartTime[i]) + landDuration[i]; 
+                int finishWaterLand = max(WS[j]+WD[j], LS[i]) + LD[i];
 
-                ans = min(time, ans);
+                ans = min({ans, finishLandWater, finishWaterLand});
             }
         }
 
