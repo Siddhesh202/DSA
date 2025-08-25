954d2277df8420ca6644557d0b9939b31e92aca3
Siddhesh Pisal <82213191+Siddhesh202@users.noreply.github.com>
2025-08-25 21:20:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (85.13%) - LeetSync

---
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};
+
+// n/2 (2a + (n-1)*d)
+// SumOdd = n/2(2+2n-2) = n^2 = n*n
+// SumEven = n/2(4+2n-2) = n/2*(2(1+n)) = n*(1+n)
