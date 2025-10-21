c5ec7597d8bd9b3c4bc826cfc7dd081c1656870b
Siddhesh Pisal <82213191+Siddhesh202@users.noreply.github.com>
2025-10-21 23:03:58 +0530

    Time: 0 ms (100%), Space: 23.3 MB (78.96%) - LeetHub

--- 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
@@ -1,39 +1,27 @@
 // Simple LPS Question
 class Solution {
 public:
-    // string longestPrefix(string s) {
-    //     int n = s.length();
-    //     vector<int> LPS(n, 0);
-    //     int i = 1, length = 0;
-
-    //     while(i < n) {
-    //         if(s[i] == s[length]) {
-    //             length++;
-    //             LPS[i] = length;
-    //             i++;
-    //         }
-    //         else {
-    //             if(length != 0)
-    //                 length = LPS[length-1];
-    //             else
-    //                 LPS[i] = 0;
-    //                 i++;            
-    //             }
-    //     }
-
-    //     return s.substr(0, LPS[n-1]);
-    // }
     string longestPrefix(string s) {
-        int n = s.size();
-        vector<int> lps(n);
-        for (int i = 1; i < n; i++) {
-            int len = lps[i - 1];
-            while (len > 0 && s[i] != s[len])
-                len = lps[len - 1];
-            if (s[i] == s[len])
-                len++;
-            lps[i] = len;
+        int n = s.length();
+        vector<int> LPS(n, 0);
+        int i = 1, length = 0;
+
+        while(i < n) {
+            if(s[i] == s[length]) {
+                length++;
+                LPS[i] = length;
+                i++;
+            }
+            else {
+                if(length != 0)
+                    length = LPS[length-1];
+                else {
+                    LPS[i] = 0;
+                    i++;            
+                }
+            }
         }
-        return s.substr(0, lps[n - 1]);
+
+        return s.substr(0, LPS[n-1]);
     }
 };
