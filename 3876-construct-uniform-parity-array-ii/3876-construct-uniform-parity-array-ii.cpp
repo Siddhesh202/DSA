73a65aedb4d90d1a2362f4160108300c6adf6cc3
Siddhesh Pisal <82213191+Siddhesh202@users.noreply.github.com>
2026-03-22 18:29:49 +0530

    Time: 6 ms (55.87%), Space: 165.8 MB (65.63%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int minOdd = INT_MAX;
+        bool hasOdd = false, canBeOdd = true, canBeEven = true;
+
+        // Find smallest odd
+        for (int num : nums1) {
+            if (num % 2 != 0) {
+                hasOdd = true;
+                minOdd = min(minOdd, num);
+            }
+        }
+
+        // If no odd → already uniform (all even)
+        if (!hasOdd) return true;
+
+        // 1. Try creating odd parity
+        for(int i = 0; i < nums1.size(); i++) {
+            int num = nums1[i];
+
+            // check if there is an odd elem smaller than this
+            if(num % 2 == 0 && minOdd > num) {
+                canBeOdd = false;
+                break;
+            }
+
+        }
+
+        // 2. Try creating even parity
+        for(int i = 0; i < nums1.size(); i++) {
+            int num = nums1[i];
+
+            // check if there is an odd elem smaller than this
+            if(num % 2 != 0 && minOdd >= num) {
+                canBeEven = false;
+                break;
+            }
+
+        }
+
+
+        return canBeOdd || canBeEven;
+    }
+};
+
+// 1. odd - odd = even
+// 2. even - even = even
+// 3. even - odd = odd
+
+// If I need to make an odd arr 2 conditions
+// 1. num should be odd itself
+// 2. if num is even, there should be an odd elem smaller than this elem
+
+// If I need to make an even arr
+// 1. num should be even itself
+// 2. if num is odd, there should be an odd elem smaller than this elem
