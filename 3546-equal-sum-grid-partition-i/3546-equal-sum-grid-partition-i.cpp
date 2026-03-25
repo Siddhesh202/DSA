80327cb7ce4a3df3f5529bf694071a68a21148b6
Siddhesh Pisal <82213191+Siddhesh202@users.noreply.github.com>
2026-03-25 23:44:45 +0530

    Time: 6 ms (75.9%), Space: 126.4 MB (93.88%) - LeetHub

---
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.cpp
@@ -0,0 +1,39 @@
+#define ll long long
+class Solution {
+public:
+
+    // TC - O(3*n)
+    // SC - O(1)
+    bool canPartitionGrid(vector<vector<int>>& grid) {
+        ll total = 0, m = grid.size(), n = grid[0].size();
+
+        // Get total sum
+        for(int row = 0; row < m; row++) {
+            for(int col = 0; col< n; col++) {
+                total += grid[row][col];
+            } 
+        }
+
+        // Try for horizontal cuts
+        ll horiSum = 0;
+        for(int row = 0; row < m; row++) {
+            for(int col = 0; col< n; col++) {
+                horiSum += grid[row][col];
+            } 
+
+            if(horiSum == (total - horiSum)) return true;
+        }
+
+        // Try for vertical cuts
+        ll vertSum = 0;
+        for(int col = 0; col < n; col++) {
+            for(int row = 0; row < m; row++) {
+                vertSum += grid[row][col];
+            } 
+
+            if(vertSum == (total - vertSum)) return true;
+        }
+
+        return false;
+    }
+};
