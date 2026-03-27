68f82c1c06327279905c6f8165ad49a409f0a9f5
Siddhesh Pisal <82213191+Siddhesh202@users.noreply.github.com>
2026-03-27 15:28:04 +0530

    Time: 2 ms (34.16%), Space: 35.1 MB (5.59%) - LeetHub

---
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool areSimilar(vector<vector<int>>& mat, int k) {
+        int rows = mat.size(), cols = mat[0].size();
+        vector<vector<int>> arr = mat;
+
+        k %= cols; // Reduce k to k%cols
+        if(k == 0) return true;
+
+        // Check what happens if there are duplicates and how to handle it
+        for(int i = 1; i <= k; i++) {
+            vector<vector<int>> temp = arr;
+            for(int row = 0; row < rows; row++) {
+                for(int col = 0; col < cols; col++) {
+                    // For even no rows, do a cyclic left
+                    if(row%2) temp[row][(col-1+cols)%cols] = arr[row][col];
+
+                    // For odd rows, do a cyclic right
+                    else temp[row][(col+1)%cols] = arr[row][col];
+                }
+            }
+
+            arr = temp;
+        }
+
+        return arr == mat;
+    }
+};
