0895a61eb49b116eb868dbd3143a546cf42ba7f2
Siddhesh Pisal <82213191+Siddhesh202@users.noreply.github.com>
2026-03-31 00:40:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3882-minimum-xor-path-in-a-grid/3882-minimum-xor-path-in-a-grid.cpp
+++ 3882-minimum-xor-path-in-a-grid/3882-minimum-xor-path-in-a-grid.cpp
@@ -1,44 +1,34 @@
 class Solution {
 public:
     int m,n;
+    void dfs(vector<vector<int>>& grid, int i, int j, int curr_xor, vector<vector<vector<bool>>> &vis) {
+        vis[i][j][curr_xor] = true;
 
-    int bfs(vector<vector<int>>& grid) {
-        vector<vector<vector<bool>>> vis(m, vector<vector<bool>>(n, vector<bool>(1024, false)));
-        queue<tuple<int,int,int>> q;
-        
-        q.push({0,0,grid[0][0]});
-        vis[0][0][grid[0][0]] = true;
+        if(i == m-1 && j == n-1) return;
 
-        while(!q.empty()) {
-            auto [i, j, curr_xor] = q.front();
-            q.pop();
-
-            // go right
-            if(j+1 < n) {
-                int new_xor = curr_xor ^ grid[i][j+1];
-                if(!vis[i][j+1][new_xor]) {
-                    q.push({i, j+1, new_xor});
-                    vis[i][j+1][new_xor] = true;
-                }
-            }
-
-            // go down
-            if(i+1 < m) {
-                int new_xor = curr_xor ^ grid[i+1][j];
-                if(!vis[i+1][j][new_xor]) {
-                    q.push({i+1, j, new_xor});
-                    vis[i+1][j][new_xor] = true;
-                }
-            }
+        // If possible go right
+        if(j+1 < n) {
+            int nx = curr_xor ^ grid[i][j+1];
+            if(!vis[i][j+1][nx]) dfs(grid, i, j+1, nx, vis);
         }
 
-        for(int i = 0; i < 1024; i++) if(vis[m-1][n-1][i]) return i; 
-        
-        return 0;
+        // If possible go down
+        if(i+1 < m) {
+            int nx = curr_xor ^ grid[i+1][j];
+            if(!vis[i+1][j][nx]) dfs(grid, i+1, j, nx, vis);
+        }
+
+        return;
     }
 
     int minCost(vector<vector<int>>& grid) {
         m = grid.size(), n = grid[0].size();
-        return bfs(grid);
+        vector<vector<vector<bool>>> vis(m, vector<vector<bool>>(n, vector<bool>(1024, false)));
+
+        dfs(grid, 0, 0, grid[0][0], vis);
+
+        for(int i = 0; i < 1024; i++) if(vis[m-1][n-1][i]) return i;
+
+        return 0;
     }
 };
