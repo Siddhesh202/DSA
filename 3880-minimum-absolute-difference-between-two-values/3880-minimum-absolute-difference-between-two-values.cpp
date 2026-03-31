cfb9308940ab68e4283d85a8de59236c80b9df5d
Siddhesh Pisal <82213191+Siddhesh202@users.noreply.github.com>
2026-04-01 00:11:15 +0530

    Time: 0 ms (100%), Space: 25.7 MB (66.52%) - LeetHub

--- 3880-minimum-absolute-difference-between-two-values/3880-minimum-absolute-difference-between-two-values.cpp
+++ 3880-minimum-absolute-difference-between-two-values/3880-minimum-absolute-difference-between-two-values.cpp
@@ -7,12 +7,12 @@ public:
             if (nums[i] == 1){
                 idx1 = i;
                 if (idx2 != -1)
-                    res = std::min(abs(idx1-idx2), res);
+                    res = min(abs(idx1-idx2), res);
             }
             else if (nums[i] == 2) {
                 idx2 = i;
                 if (idx1 != -1)
-                    res = std::min(abs(idx1-idx2), res);
+                    res = min(abs(idx1-idx2), res);
             }
         }
         return (res == INT_MAX) ? -1: res;
