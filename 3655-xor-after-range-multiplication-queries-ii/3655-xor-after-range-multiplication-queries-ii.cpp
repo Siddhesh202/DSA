111db4ca44681ebdf33424a19643d92ec4970af9
Siddhesh Pisal <82213191+Siddhesh202@users.noreply.github.com>
2026-04-11 02:06:46 +0530

    Time: 459 ms (45.87%), Space: 430.4 MB (19%) - LeetHub

--- 3655-xor-after-range-multiplication-queries-ii/3655-xor-after-range-multiplication-queries-ii.cpp
+++ 3655-xor-after-range-multiplication-queries-ii/3655-xor-after-range-multiplication-queries-ii.cpp
@@ -1,57 +1,74 @@
+#define MOD 1000000007
+#define ll long long
 class Solution {
-    const int mod = 1e9 + 7;
-    using ll = long long;
-    int pow(ll x, ll y) {
-        ll res = 1;
-        for (; y; y >>= 1) {
-            if (y & 1) {
-                res = res * x % mod;
-            }
-            x = x * x % mod;
-        }
-        return res;
+public:
+    int n;
+    ll pow(int a, int b) {
+        if(b == 0) return 1;
+
+        ll half = pow(a, b/2);
+        ll ans = (half * half) % MOD;
+
+        if(b % 2 != 0) ans = (ans * a) % MOD;
+        return ans;
     }
 
-public:
+    ll fermatTheorem(int a, int b) { // need to do (a / b) % MOD --> a * (b ^ M-2) % MOD 
+        ll x = a;
+        ll y = pow(b, MOD-2) % MOD;
+        return (x * y) % MOD;
+    }
+
+    void DAT(vector<int> &nums, int k, vector<vector<int>>& queries) {
+        vector<ll> diff(n, 1);
+
+        // populate diff arr
+        for(auto &query : queries) {
+            int l = query[0], r = query[1], v = query[2];
+
+            diff[l] = (diff[l] * v) % MOD;
+            int steps = (r - l) / k;
+            int next = l + (steps + 1) * k;
+
+            if(next < n) diff[next] = fermatTheorem(diff[next], v); // do (diff[next] / val) % MOD
+        }
+
+        //Cumulative product
+        for(int i = 0; i < n; i++) {
+            if(i-k >= 0)
+                diff[i] = (diff[i] * diff[i-k]) % MOD;
+        }
+
+        //Apply diff to nums
+        for(int i = 0; i < n; i++) {
+            nums[i] = (1LL * nums[i] * diff[i]) % MOD;
+        }
+    }
+
     int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
-        int n = nums.size();
-        int T = sqrt(n);
-        vector<vector<vector<int>>> groups(T);
-        for (auto& q : queries) {
-            int l = q[0], r = q[1], k = q[2], v = q[3];
-            if (k < T) {
-                groups[k].push_back({l, r, v});
-            } else {
-                for (int i = l; i <= r; i += k) {
-                    nums[i] = 1ll * nums[i] * v % mod;
-                }
+        n = nums.size();
+        int blockSize = ceil(sqrt(n)), ans = 0;
+        unordered_map<int,vector<vector<int>>> smallKMap;
+
+        for(auto &query : queries) {
+            int l = query[0], r = query[1], k = query[2], v = query[3];
+
+            if(k >= blockSize) { // if k is large do square root decomposition
+                for(int i = l; i <= r; i +=k) nums[i] = (1LL * nums[i] * v) % MOD;
+            }
+            else { // Apply Difference Array Technique with Jumps for smaller k
+                smallKMap[k].push_back({l, r, v});
             }
         }
 
-        vector<ll> dif(n + T);
-        for (int k = 1; k < T; k++) {
-            if (groups[k].empty()) {
-                continue;
-            }
-            fill(dif.begin(), dif.end(), 1);
-            for (auto& q : groups[k]) {
-                int l = q[0], r = q[1], v = q[2];
-                dif[l] = dif[l] * v % mod;
-                int R = ((r - l) / k + 1) * k + l;
-                dif[R] = dif[R] * pow(v, mod - 2) % mod;
-            }
+        // process elements in small k using differnce array technique
+        for(auto &[k, smallQueries] : smallKMap) {
+            DAT(nums, k, smallQueries);
+        } 
 
-            for (int i = k; i < n; i++) {
-                dif[i] = dif[i] * dif[i - k] % mod;
-            }
-            for (int i = 0; i < n; i++) {
-                nums[i] = 1ll * nums[i] * dif[i] % mod;
-            }
-        }
-        int res = 0;
-        for (int i = 0; i < n; i++) {
-            res = res ^ nums[i];
-        }
-        return res;
+        // finally do the XOR of all elements
+        for(int i = 0; i < n; i++) ans ^= nums[i];
+
+        return ans;
     }
 };
