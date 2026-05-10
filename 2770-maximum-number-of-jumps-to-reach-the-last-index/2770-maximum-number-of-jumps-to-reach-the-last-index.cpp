f9d7a8ea491a43b066485eb07ca5d1af1588a051
Siddhesh Pisal <82213191+Siddhesh202@users.noreply.github.com>
2026-05-10 10:50:46 +0530

    Time: 22 ms (45.96%), Space: 68.4 MB (84.78%) - LeetHub

---
+++ 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maximumJumps(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<int> dp(n, -1); // -1 means unreachable state from 0
+        dp[0] = 0;
+
+        for(int i = 1; i < n; i++) {
+
+            for(int j = 0; j < i; j++) {
+
+                int diff = nums[j] - nums[i];
+
+                if(dp[j] != -1 && diff >= -target && diff <= target) 
+                    dp[i] = max(dp[i], 1 + dp[j]);
+            }
+        }
+
+        return dp[n-1] == 0 ? -1 : dp[n-1];
+    }
+};
+
+// dp
+// 1 3 6 4 1 2
+// 0 1 0 2 2 3
