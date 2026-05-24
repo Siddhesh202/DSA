d3e4914ee5833bfe81f594853b36071432397955
Siddhesh Pisal <82213191+Siddhesh202@users.noreply.github.com>
2026-05-24 12:18:32 +0530

    Time: 23 ms (22.32%), Space: 22.3 MB (14.83%) - LeetHub

---
+++ 1340-jump-game-v/1340-jump-game-v.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int n;
+    int dp[1001][1001];
+    int solve(vector<int>& arr, int i, int d) {
+        
+        if(dp[i][d] != -1) return dp[i][d];
+
+        int left = 0, right = 0;
+
+        // solve for left
+        for(int j = i-1; j >= max(0, i-d); j--) {
+            if(arr[j] >= arr[i]) break;
+            left = max(left, solve(arr, j, d));
+        }
+
+        // solve for right
+        for(int j = i+1; j <= min(n-1, i+d); j++) {
+            if(arr[j] >= arr[i]) break;
+            right = max(right, solve(arr, j, d));
+        }
+
+        return dp[i][d] = 1 + max(left, right);
+    }
+
+    int maxJumps(vector<int>& arr, int d) {
+        n = arr.size();
+        int ans = 0;
+        memset(dp, -1, sizeof(dp));
+
+        for(int i = 0; i < n; i++) {
+            ans = max(ans, solve(arr, i, d));
+        }
+
+        return ans;
+    }
+};
