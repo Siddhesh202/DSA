48346cb07a6d7af2cf652654f0158d0f9273a5fe
Siddhesh Pisal <82213191+Siddhesh202@users.noreply.github.com>
2026-06-13 10:54:40 +0530

    Time: 0 ms (100%), Space: 43.5 MB (77.6%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans = "";
+
+        for(auto &word : words) {
+            int total = 0;
+            for(auto &ch : word) total += (weights[ch - 'a']);
+            // cout << total << endl;
+            total = total % 26;
+            ans.push_back('z'-total);
+        }
+
+        return ans;
+    }
+};
