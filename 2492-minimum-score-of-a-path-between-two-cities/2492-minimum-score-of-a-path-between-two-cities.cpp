6f217ca864a4ea8735280bd13677f7ac115cec9a
Siddhesh Pisal <82213191+Siddhesh202@users.noreply.github.com>
2026-07-04 12:49:35 +0530

    Time: 8 ms (98.01%), Space: 107.4 MB (96.89%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,28 +1,46 @@
 class Solution {
 public:
-    int mini = INT_MAX;
-    void dfs(unordered_map<int,vector<vector<int>>> &adj, vector<bool> &visited, int node) {
-        visited[node] = true;
-        
-        for(auto &neig: adj[node]) {
-            int adjNode = neig[0];
-            int dist = neig[1];
+    vector<int> parent, rank;
 
-            if(!visited[adjNode]) dfs(adj, visited, adjNode);
-            mini = min(mini, dist);
+    void Union(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+
+        if(pa == pb) return;
+
+        if(rank[pa] < rank[pb]) 
+            parent[pa] = pb;
+        else if(rank[pb] < rank[pa]) 
+            parent[pb] = pa;
+        else {
+            parent[pa] = pb;
+            rank[pb]++;
         }
     }
 
+    int find(int x) {
+        if(x == parent[x]) return x;
+
+        return parent[x] = find(parent[x]);
+    }
+
     int minScore(int n, vector<vector<int>>& roads) {
-        unordered_map<int,vector<vector<int>>> adj;
-        vector<bool> visited(n+1, false);
+        parent.resize(n + 1);
+        rank.assign(n + 1, 0);
+
+        for(int i = 1; i <= n; i++) parent[i] = i;
+
+        // create a DSU
+        for(auto &road : roads) Union(road[0], road[1]);
+
+        // Now find mini
+        int root = find(1);
+        int ans = INT_MAX;
 
         for(auto &road : roads) {
-            adj[road[0]].push_back({road[1], road[2]});
-            adj[road[1]].push_back({road[0], road[2]});
+            if(find(road[0]) == root) ans = min(ans, road[2]);
         }
 
-        dfs(adj, visited, 1);
-        return mini;
+        return ans;
     }
 };
